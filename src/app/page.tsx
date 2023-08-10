c46242c9bf80b327a2fe2844a14c3337890f6864
slyguy5646 <lpmonaghan06@yahoo.com>
2023-08-10 13:03:45 -0400

    fixed lander bottom bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,30 +13,37 @@ import ItsEmpty from "@/components/ItsEmpty";
 
 function Home() {
     return (
-        <div className="">
-            <div className="flex justify-center items-center ">
-                <div className="  w-full lg:max-w-[66.6667%] flex flex-col  items-start gap-y-8 px-12">
-                    <Nav />
-                    <div className="text-xl font-normal self-center justify-self-center">
-                        Hey!
-                        <span className="font-bold"> I&apos;m Liam.</span>{" "}
-                        I&apos;m a Christian, student, developer,{" "}
-                        <A href="/photos">photographer</A>, roboticist,
-                        Pittsburgh and Ohio State sports fan, and an avid movie
-                        and music consumer. You can usually find me coding, with
-                        my{" "}
-                        <A href="https://frc8592.org">awesome robotics team</A>,{" "}
-                        <A href="/blog">writing about something</A>, or hanging
-                        out with friends.
-                    </div>
-                    <div className="pt-6 text-2xl font-semibold">
-                        Recent Blog Posts
-                    </div>
-                    {sortPostsNewestToOldest(allPosts.slice(0, 2)).length !=
-                    0 ? (
-                        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 w-full pb-6  ">
-                            {sortPostsNewestToOldest(allPosts.slice(0, 2)).map(
-                                (post) => (
+        <div className="min-h-screen bg-cover bg-center flex items-center justify-center bg-background">
+            <div className="container mx-auto p-4 min-h-screen">
+                <div className="flex justify-center items-center ">
+                    <div className="  w-full lg:max-w-[66.6667%] flex flex-col  items-start gap-y-8 px-12">
+                        <Nav />
+                        <div className="text-xl font-normal self-center justify-self-center">
+                            Hey!
+                            <span className="font-bold">
+                                {" "}
+                                I&apos;m Liam.
+                            </span>{" "}
+                            I&apos;m a Christian, student, developer,{" "}
+                            <A href="/photos">photographer</A>, roboticist,
+                            Pittsburgh and Ohio State sports fan, and an avid
+                            movie and music consumer. You can usually find me
+                            coding, with my{" "}
+                            <A href="https://frc8592.org">
+                                awesome robotics team
+                            </A>
+                            , <A href="/blog">writing about something</A>, or
+                            hanging out with friends.
+                        </div>
+                        <div className="pt-6 text-2xl font-semibold">
+                            Recent Blog Posts
+                        </div>
+                        {sortPostsNewestToOldest(allPosts.slice(0, 2)).length !=
+                        0 ? (
+                            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 w-full pb-6  ">
+                                {sortPostsNewestToOldest(
+                                    allPosts.slice(0, 2)
+                                ).map((post) => (
                                     <BlogCard
                                         key={post.slug}
                                         title={post.title}
@@ -45,12 +52,12 @@ function Home() {
                                         slug={post.slug}
                                         categories={post.categories}
                                     />
-                                )
-                            )}
-                        </div>
-                    ) : (
-                        <ItsEmpty />
-                    )}
+                                ))}
+                            </div>
+                        ) : (
+                            <ItsEmpty />
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
