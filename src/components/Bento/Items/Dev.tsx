f34ec0aa16591b55ed698c8ec89e7f53d4256440
slyguy5646 <lpmonaghan06@yahoo.com>
2023-09-04 15:19:52 -0400

    dev bento done

--- src/components/Bento/Items/Dev.tsx
+++ src/components/Bento/Items/Dev.tsx
@@ -7,7 +7,14 @@ import { IconArrowRight } from "@tabler/icons-react";
 export default function DevBento() {
     return (
         <div className="relative h-[325px] md:h-full overflow-hidden">
-            <div className="text-xl font-bold">Dev</div>
+            <div className="flex justify-between  md:justify-normal md:gap-x-2 gap-x-0  items-center pb-2">
+                <div className="text-xl font-bold">Dev</div>
+                <div className="text-xl font-bold text-custom-500">
+                    <a href="/dev">
+                        <IconArrowRight />
+                    </a>
+                </div>
+            </div>
             <div className="md:max-w-[55%] lg:max-w-[50%] md:flex flex-col gap-y-2">
                 <div className="md:mb-0 mb-4">
                     I have experience in several areas. Some of my most
@@ -27,32 +34,13 @@ export default function DevBento() {
                     >
                         PuppoPool
                     </a>
-                    . My primary skillset includes React, Tailwind, NextJS, Sanity, Typescript,
-                    Prisma, and{" "}
+                    . My primary skillset includes React, Tailwind, NextJS,
+                    Sanity, Typescript, Prisma, and{" "}
                     <a href="/stack" className="text-blue-200 font-semibold">
                         more
                     </a>
                     .
                 </div>
-                <Button
-                    href="/dev"
-                    className=" absolute md:hidden bottom-2 right-4 "
-                >
-                    <div className="flex items-center gap-x-2 text-black ">
-                        <div className=" md:text-lg font-semibold ">
-                            See portfolio
-                        </div>
-                        <IconArrowRight />
-                    </div>
-                </Button>
-                <Button href="/dev" className=" hidden md:block ">
-                    <div className="flex items-center gap-x-2 text-black ">
-                        <div className=" md:text-lg font-semibold ">
-                            See portfolio
-                        </div>
-                        <IconArrowRight />
-                    </div>
-                </Button>
             </div>
 
             <MiniCodeEditor />
@@ -68,7 +56,7 @@ const code = `export class LPM {
 
 function MiniCodeEditor() {
     return (
-        <div className="absolute w-full  md:h-full -bottom-4 md:-bottom-6 right-0 md:-right-6 border-2 border-slate-400/10 p-4 rounded-xl md:max-w-[45%] lg:max-w-[50%]">
+        <div className="absolute w-full  md:h-full -bottom-4 md:-bottom-6 right-0 md:-right-6 border-2 border-slate-400/10 p-4 rounded-xl md:max-w-[45%] lg:max-w-[50%] bg-neutral-800">
             <CodeText code={code} />
         </div>
     );
