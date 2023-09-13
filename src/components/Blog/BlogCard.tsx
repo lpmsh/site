dae004e8f8f9db02b18cbe3d5ef151b21d3c18a9
slyguy5646 <lpmonaghan06@yahoo.com>
2023-09-12 22:43:33 -0400

    fixed blog card

--- src/components/Blog/BlogCard.tsx
+++ src/components/Blog/BlogCard.tsx
@@ -2,11 +2,12 @@ import * as React from "react";
 import { getHumanDateFromSlashDate } from "@/utils/DateUtils";
 import { PostWithoutContent } from "@/sanity/post";
 import { SanityImageComponent } from "@/sanity/utils/SanityImageComponent";
+import Card from "../Card";
 
 function BlogCard({ title, _createdAt, icon, slug }: PostWithoutContent) {
     return (
         <a href={`/blog/${slug}`}>
-            <div className=" hover:border-neutral-600 border-neutral-800 border-2 rounded-xl bg-neutral-800 px-8 py-6 text-left w-full">
+            <Card className="px-8 py-6 text-left w-full">
                 <div className="flex flex-col gap-y-2  justify-between items-center">
                     <SanityImageComponent value={icon} className="w-[64px] h-[64px]" />
                     <div className="flex flex-col ">
@@ -14,7 +15,7 @@ function BlogCard({ title, _createdAt, icon, slug }: PostWithoutContent) {
                         <div className="font-light text-md  text-center">{getHumanDateFromSlashDate(_createdAt)}</div>
                     </div>
                 </div>
-            </div>
+            </Card>
         </a>
     );
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,6 @@
+import { cn } from "@/utils/cn";
+import { ReactNode } from "react";
+
+export default function Card({children, className}:{children: ReactNode, className?: string}){
+    return <div className={cn("rounded-xl border-2 border-slate-400/10 bg-neutral-100 p-6 dark:bg-neutral-900", className)}>{children}</div>;
+}
