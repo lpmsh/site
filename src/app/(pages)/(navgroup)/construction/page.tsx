9de4bfac115fd47d5b38721ed350310c8f444710
slyguy5646 <lpmonaghan06@yahoo.com>
2023-09-20 23:04:15 -0400

    cleaned up construction page responsiveness

--- src/app/(pages)/(navgroup)/construction/page.tsx
+++ src/app/(pages)/(navgroup)/construction/page.tsx
@@ -3,10 +3,10 @@ import { IconHome } from "@tabler/icons-react";
 
 export default function ConstructionPage() {
     return (
-        <div className="flex justify-center h-[70dvh]">
-            <div className="flex flex-col justify-center items-center gap-y-6">
+        <div className="flex justify-center">
+            <div className="flex flex-col justify-center items-center gap-y-6 mt-4">
                 <div className="text-8xl">🚨</div>
-                <div className="text-8xl font-bold text-center">This page is under construction.</div>
+                <div className="text-4xl md:text-8xl font-bold text-center">This page is under construction.</div>
 
                 <a href="/">
                     <div className="mt-4 flex gap-x-2 jusitfy-center items-center bg-custom-500 w-fit p-2 rounded-md text-black">
