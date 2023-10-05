6410489ce756ec078dea21aace21a942f879e869
slyguy5646 <lpmonaghan06@yahoo.com>
2023-10-05 12:23:13 +0800

    added text scroll on code sample

--- src/components/CodeText.tsx
+++ src/components/CodeText.tsx
@@ -5,8 +5,10 @@ import "../app/highlight.css"
 export default function CodeText({ code, showLineNumbers }: { code: string; showLineNumbers?: boolean}) {
     return (
         //style={atomOneDark}
-        <SyntaxHighlighter language="typescript" useInlineStyles={false} showLineNumbers={showLineNumbers} startingLineNumber={25}>
-            {code}
-        </SyntaxHighlighter>
+        <div className="overflow-scroll">
+            <SyntaxHighlighter language="typescript" useInlineStyles={false} showLineNumbers={showLineNumbers} startingLineNumber={25}>
+                {code}
+            </SyntaxHighlighter>
+        </div>
     );
 }
