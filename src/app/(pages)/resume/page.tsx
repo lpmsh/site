aa505a0422bc3f2f053007ca6c25a33e1d65c0c8
slyguy5646 <lpmonaghan06@yahoo.com>
2025-09-04 22:16:08 -0400

    added resume page

---
+++ src/app/(pages)/resume/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from "next/navigation";
+
+export default function Resume() {
+  redirect("https://drive.google.com/file/d/1vfXgtGP9a-16aVhlGEMxyTSc75h8kIlC/view?usp=sharing");
+
+  return null;
+}
