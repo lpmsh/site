7de0c0d9e2d5ae5cffb4b03b8bea20db7a4dd30f
Liam Monaghan <monaghal@purdue.edu>
2025-11-04 10:12:16 -0500

    updated resume link

--- src/app/(pages)/resume/page.tsx
+++ src/app/(pages)/resume/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 
 export default function Resume() {
-  redirect("https://drive.google.com/file/d/1vfXgtGP9a-16aVhlGEMxyTSc75h8kIlC/view?usp=sharing");
+  redirect("https://drive.google.com/file/d/1MX8_0GAdw38xbpSF5pYfAHe9xvkAFs4h/view?usp=sharing");
 
   return null;
 }
