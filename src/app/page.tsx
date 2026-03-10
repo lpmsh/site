493d011203da625c93d20475a7bc1cc658f2237e
slyguy5646 <lpmonaghan06@gmail.com>
2026-03-10 15:54:55 -0400

    homepage copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,38 +13,19 @@ export default function Home() {
         </div>
 
         <div className="pt-8">
-          I&apos;m a Christian, student, developer,{" "}
+          Christian. Developer.{" "}
           <a href="/photos" className="text-neutral-500 hover:text-neutral-600 underline">
-            photographer
+            Photographer
           </a>
-          , Pittsburgh and Ohio State sports fan, and can't live without music. You can usually find me{" "}
-          <a href="https://github.com/lpmsh" target="_blank" className="text-neutral-500 hover:text-neutral-600 underline">
-            coding
-          </a>{" "}
-          or hanging out with friends. I also occasionally{" "}
-          <a href="/blog" className="text-neutral-500 hover:text-neutral-600 underline">
-            write about things
-          </a>
-          .
-        </div>
-
-        {/*<div className="pt-8">
-          I'm currently hacking on{" "}
-          <a href="https://promptiac.com?utm_source=lpmsh" target="_blank" className="text-blue-500 hover:text-opacity-70">
-            Promptiac
-          </a>
-          .
-        </div>*/}
-
-        <div className="pt-8">
-          I'm currently studying computer science at{" "}
+          . Studying CS at{" "}
           <a href="https://purdue.edu" target="_blank" className="text-[#CFB991] hover:text-opacity-70 underline">
             Purdue
           </a>
           .
         </div>
-        <div className="pt-2">
-          I was previously a software engineer intern for{" "}
+
+        <div className="pt-8">
+          Previously built software at{" "}
           <a href="https://flowglad.com" target="_blank" className="text-[#DD7D2A] hover:text-opacity-70 underline">
             Flowglad
           </a>{" "}
@@ -52,13 +33,17 @@ export default function Home() {
           <a href="https://roda.com" target="_blank" className="text-[#1D6CDC] hover:text-opacity-70 underline">
             Roda
           </a>
-          .
-        </div>
-
-        <div className="pt-8">
-          Wanna chat?{" "}
+          . I{" "}
+          <a href="https://github.com/lpmsh" target="_blank" className="text-neutral-500 hover:text-neutral-600 underline">
+            code
+          </a>
+          ,{" "}
+          <a href="/blog" className="text-neutral-500 hover:text-neutral-600 underline">
+            write
+          </a>
+          , and{" "}
           <Link className="text-neutral-500 hover:text-neutral-600 underline" target="_blank" href={"https://cal.com/liammonaghan/30min"}>
-            Book a call
+            chat
           </Link>
           .
         </div>
