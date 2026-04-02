4442edd7ec10d5de7a9b3ee736e1d04635813238
slyguy5646 <lpmonaghan06@gmail.com>
2026-04-02 00:05:48 -0400

    switch social icons to radix-ui, remove substack link

--- src/components/Nav/SocialLinks.tsx
+++ src/components/Nav/SocialLinks.tsx
@@ -1,4 +1,4 @@
-import { Github, Linkedin, Instagram, Twitter } from "lucide-react";
+import { GitHubLogoIcon, LinkedInLogoIcon, InstagramLogoIcon, TwitterLogoIcon } from "@radix-ui/react-icons";
 
 export default function SocialLinks() {
   const iconClass = "w-3.5 h-3.5";
@@ -6,21 +6,16 @@ export default function SocialLinks() {
   return (
     <div className="flex items-center gap-x-3 w-fit">
       <a className="text-neutral-300 transition hover:text-neutral-500" href="https://github.com/lpmsh" target="_blank">
-        <Github className={iconClass} />
+        <GitHubLogoIcon className={iconClass} />
       </a>
       <a className="text-neutral-300 transition hover:text-neutral-500" href="https://www.linkedin.com/in/liampmonaghan/" target="_blank">
-        <Linkedin className={iconClass} />
+        <LinkedInLogoIcon className={iconClass} />
       </a>
       <a className="text-neutral-300 transition hover:text-neutral-500" href="https://instagram.com/lpm_visuals" target="_blank">
-        <Instagram className={iconClass} />
+        <InstagramLogoIcon className={iconClass} />
       </a>
       <a className="text-neutral-300 transition hover:text-neutral-500" href="https://twitter.com/lmon_25" target="_blank">
-        <Twitter className={iconClass} />
-      </a>
-      <a className="text-neutral-300 transition hover:text-neutral-500" href="https://substack.com/@lpmsh" target="_blank">
-        <svg role="img" viewBox="0 0 24 24" className={iconClass} stroke="currentColor" strokeWidth="2" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M22.539 8.242H1.46V5.406h21.08v2.836zM1.46 10.812V24L12 18.11 22.54 24V10.812H1.46zM22.54 0H1.46v2.836h21.08V0z" fill="currentColor" stroke="none" />
-        </svg>
+        <TwitterLogoIcon className={iconClass} />
       </a>
     </div>
   );
