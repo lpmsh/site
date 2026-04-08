3cb2fc8269ad8cd5fa1e9621a3fd71acae2c527d
slyguy5646 <lpmonaghan06@gmail.com>
2026-04-08 03:30:00 -0400

    update resume redirect to rezume.so/liam

--- src/app/(pages)/resume/page.tsx
+++ src/app/(pages)/resume/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 
 export default function Resume() {
-  redirect("https://drive.google.com/file/d/1xLAH2ARBs1itgR76YKWykLgoovZClGvw/view?usp=sharing");
+  redirect("https://rezume.so/liam");
 
   return null;
 }
